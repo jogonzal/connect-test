514fb9b14726bbea84234f96204e3b7ffe49b566
Jorge <jogonzal@users.noreply.github.com>
2022-09-29 23:28:37 -0700

    Fix lint

--- pages/api/checkout-session.ts
+++ pages/api/checkout-session.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import type { NextApiRequest, NextApiResponse } from "next";
 import { hostUrl } from "../../config/EnvironmentVariables";
 import { StripeClient } from "../../config/StripeUtils";
--- pages/api/create-dashboard-login-link.ts
+++ pages/api/create-dashboard-login-link.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import type { NextApiRequest, NextApiResponse } from "next";
 import { StripeClient } from "../../config/StripeUtils";
 
