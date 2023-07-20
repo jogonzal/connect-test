ed4b163bcbcaac4417cfa76ea844df3b344e4398
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2023-07-20 15:21:07 -0700

    Update OnboardingExperience.tsx

--- components/OnboardingExperience.tsx
+++ components/OnboardingExperience.tsx
@@ -2,23 +2,11 @@ import * as React from "react";
 import { ConnectAccountOnboarding } from "../hooks/ConnectJsTypes";
 
 export const OnboardingExperienceExample: React.FC = () => {
-  const [accountOnboarded, setAccountOnboarded] =
-    React.useState<boolean>(false);
-  const [chargesEnabled, setChargesEnabled] = React.useState<boolean>(false);
-
-  if (accountOnboarded) {
-    return (
-      <p>
-        The account has onboarded! Charges enabled:{" "}
-        {chargesEnabled ? "true" : "false"}
-      </p>
-    );
-  }
-
-  const onOnboardingExited = () => {
-    setAccountOnboarded(true);
-    setChargesEnabled(true);
-  };
-
-  return <ConnectAccountOnboarding onOnboardingExited={onOnboardingExited} />;
+  return (
+    <ConnectAccountOnboarding
+      onOnboardingExited={() => {
+        console.log("The account has exited onboarding");
+      }}
+    />
+  );
 };
